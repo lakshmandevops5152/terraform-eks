5570dd149e69a6510ccde5f441a21ca69c9f48aa
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-28 11:28:52 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "lakshman-trraform"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
