444c60bedfdf5eb9dab943420a71a2c90243361d
Lakshman <malapatilakshman@gmail.com>
2025-09-28 11:22:34 +0530

    added eks terraform files

--- eks/main.tf
+++ eks/main.tf
@@ -1,21 +1,14 @@
-#################################
-# Get available AZs
-#################################
-data "aws_availability_zones" "azs" {}
-
-#################################
-# VPC Module
-#################################
+#Vpc
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = "jenkins-vpc"
+  name = "eks_cluster_vpc"
   cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.azs.names
-  private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
 
   enable_dns_hostnames = true
   enable_nat_gateway   = true
@@ -24,60 +17,46 @@ module "vpc" {
   tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
   }
-
   public_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/elb"               = 1
-  }
 
+  }
   private_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"      = 1
+    "kubernetes.io/role/private_elb"       = 1
+
   }
 }
 
-#################################
-# EKS Module
-#################################
+#EKS
+
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
-
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.29"
-
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.29"
   cluster_endpoint_public_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     nodes = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_types = ["t2.small"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = var.instance_types
     }
   }
-
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
 
-#################################
-# Variables
-#################################
-variable "vpc_cidr" {
-  default = "192.168.0.0/16"
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
 }
 
-variable "private_subnets" {
-  default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
-}
-
-variable "public_subnets" {
-  default = ["192.168.4.0/24", "192.168.5.0/24", "192.168.6.0/24"]
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
