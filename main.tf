28e5c237e2bd0985d3dd1de48c34627725352b42
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-10-08 12:29:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ resource "aws_eks_node_group" "eks_nodes" {
     min_size     = 1
   }
 
-  instance_types = ["t2. micro"]
+  instance_types = ["t3. micro"]
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node_policy,
