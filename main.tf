33f20cf7c90c2d90eeaf2255ce808a1141bd5d96
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-10-09 13:45:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -160,58 +160,70 @@ resource "aws_eks_cluster" "eks_cluster" {
 }
 
 # -------------------------------
-# 5. Auto Scaling Group for Worker Nodes
+# 5. Create Auto Scaling Group for Worker Nodes
 # -------------------------------
-resource "aws_launch_template" "eks_launch_template" {
-  name_prefix   = "eks-node-lt-"
+
+# Security Group for worker nodes
+resource "aws_security_group" "worker_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+  description = "Security group for worker nodes"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "eks-worker-sg"
+  }
+}
+
+# Launch configuration for Auto Scaling Group
+resource "aws_launch_configuration" "eks_worker_lc" {
+  name_prefix   = "eks-worker-lc-"
   image_id      = "ami-02d26659fd82cf299"
   instance_type = "t3.medium"
   key_name      = "windows"
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = [aws_security_group.eks_cluster_sg.id]
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "eks-node"
-    }
-  }
-
-  user_data = base64encode(<<-EOF
-              #!/bin/bash
-              /etc/eks/bootstrap.sh ${aws_eks_cluster.eks_cluster.name}
-              EOF
-  )
-}
-
-resource "aws_autoscaling_group" "eks_asg" {
-  name                      = "eks-node-asg"
-  desired_capacity          = 2
-  max_size                  = 3
-  min_size                  = 1
-  vpc_zone_identifier       = [aws_subnet.eks_subnet1.id, aws_subnet.eks_subnet2.id]
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
-
-  launch_template {
-    id      = aws_launch_template.eks_launch_template.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "eks-node"
-    propagate_at_launch = true
-  }
+  security_groups = [aws_security_group.worker_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.eks_node_profile.name
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+# IAM Instance Profile for Worker Nodes
+resource "aws_iam_instance_profile" "eks_node_profile" {
+  name = "eks-node-instance-profile"
+  role = aws_iam_role.eks_node_role.name
+}
+
+# Auto Scaling Group for worker nodes
+resource "aws_autoscaling_group" "eks_worker_asg" {
+  name                      = "eks-worker-asg"
+  max_size                  = 3
+  min_size                  = 1
+  desired_capacity           = 2
+  launch_configuration       = aws_launch_configuration.eks_worker_lc.id
+  vpc_zone_identifier        = [aws_subnet.eks_subnet1.id, aws_subnet.eks_subnet2.id]
+  health_check_type          = "EC2"
+  force_delete               = true
+
+  tag {
+    key                 = "Name"
+    value               = "eks-worker-node"
+    propagate_at_launch = true
+  }
 
   depends_on = [
+    aws_launch_configuration.eks_worker_lc,
     aws_eks_cluster.eks_cluster
   ]
+}
 }
