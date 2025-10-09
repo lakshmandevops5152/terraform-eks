79cfbdf41c521f07cdf87ee8446e7ec939f93123
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-10-09 15:15:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # -------------------------------
-# VPC and Subnet
+# VPC
 # -------------------------------
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -12,14 +12,23 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
-resource "aws_subnet" "eks_subnet" {
+# -------------------------------
+# Subnets in 2 AZs
+# -------------------------------
+resource "aws_subnet" "eks_subnet_a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
-  tags = {
-    Name = "eks-subnet"
-  }
+  tags = { Name = "eks-subnet-a" }
+}
+
+resource "aws_subnet" "eks_subnet_b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "eks-subnet-b" }
 }
 
 # -------------------------------
@@ -27,7 +36,6 @@ resource "aws_subnet" "eks_subnet" {
 # -------------------------------
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks_cluster_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -45,7 +53,6 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 
 resource "aws_iam_role" "eks_worker_role" {
   name = "eks_worker_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -66,76 +73,4 @@ resource "aws_iam_role_policy_attachment" "ec2_container_registry_policy" {
   role       = aws_iam_role.eks_worker_role.name
 }
 
-resource "aws_iam_instance_profile" "eks_worker_profile" {
-  name = "eks_worker_profile"
-  role = aws_iam_role.eks_worker_role.name
-}
-
-# -------------------------------
-# Security Group
-# -------------------------------
-resource "aws_security_group" "eks_worker_sg" {
-  name        = "eks_worker_sg"
-  description = "Allow all inbound traffic for EKS worker nodes"
-  vpc_id      = aws_vpc.eks_vpc.id
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-  }
-}
-
-# -------------------------------
-# EKS Cluster
-# -------------------------------
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.eks_subnet.id]
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
-}
-
-# -------------------------------
-# Launch Configuration
-# -------------------------------
-resource "aws_launch_configuration" "eks_worker" {
-  name_prefix          = "eks-worker-"
-  image_id             = "ami-0360c520857e3138f" # ubuntu EKS AMI (replace with latest in ap-south-1)
-  instance_type        = "t3.medium"
-  security_groups      = [aws_security_group.eks_worker_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.eks_worker_profile.name
-}
-
-# -------------------------------
-# Auto Scaling Group
-# -------------------------------
-resource "aws_autoscaling_group" "eks_asg" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 1
-  vpc_zone_identifier  = [aws_subnet.eks_subnet.id]
-  launch_configuration = aws_launch_configuration.eks_worker.id
-
-  health_check_type          = "EC2"
-  health_check_grace_period  = 300
-  force_delete               = true
-
-  tag {
-    key                 = "Name"
-    value               = "eks-worker-node"
-    propagate_at_launch = true
-  }
-}
+resource "aws_iam_instance_profile" "eks_worker_profile"_
